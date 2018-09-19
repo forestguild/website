dfc0d728d4f0dffec932ae16234d9b5721440f7a
Nikita Chernyi <developer.nikus@gmail.com>
2018-09-19 12:44:35 +0300

    News - added updater for raider.io, website - fixed navbar collapse on raid form on mobile

--- news.php
+++ news.php
@@ -1,44 +1,70 @@
 <?php
 
+declare(strict_types=1);
 /**
- * WoW Guild news downloader
+ * WoW Guild news downloader.
+ *
  * @author Nikita Chernyi
  */
-class News
+class news
 {
     /**
-     * News data
+     * News data.
+     *
      * @var array
      */
     protected $data = [];
     /**
-     * WoWProgress RSS url
+     * WoWProgress guild uri.
+     *
      * @var string
      */
     protected $wowprogressUrl;
 
     /**
-     * Battle.net API url
+     * Battle.net API url.
+     *
      * @var string
      */
     protected $battlenetUrl;
+
     /**
-     * Init
-     * @param string $region WoW region, default: eu
-     * @param string $realm WoW realm (server)
-     * @param string $guild Guild name
-     * @param string $apikey Battle.net API key
-     * @param string $lang Battle.net locale, default: en_GB
+     * Raider.IO payload to update guild.
+     *
+     * @var string
      */
-    public function __construct(string $region = 'eu', string $realm, string $guild, string $apikey, string $lang = 'en_GB')
+    protected $raiderioPayload;
+
+    /**
+     * Init.
+     *
+     * @param string $region WoW region, default: eu
+     * @param array  $realm  WoW realm (server) with en and ru and id, example: ['ru' => 'Галакронд', 'en' => 'Galakrond', id => 607]
+     * @param string $guild  Guild name
+     * @param string $apikey Battle.net API key
+     * @param string $lang   Battle.net locale, default: en_GB
+     */
+    public function __construct(string $region, array $realm, string $guild, string $apikey, string $lang = 'en_GB')
     {
-        if(!$apikey) {
+        if (!$apikey) {
             $this->log('Battle.net', 'warning. No API key');
         }
-        $this->wowprogressUrl = 'guild/'.$region.'/'.strtolower($realm).'/'.str_replace(' ','+',$guild);
-        $this->battlenetUrl = 'https://'.$region.'.api.battle.net/wow/guild/'.ucfirst($realm).'/'.str_replace(' ','%20', $guild).'?fields=news&locale='.$lang.'&apikey='.$apikey;
+        $this->wowprogressUrl = 'guild/'.$region.'/'.\strtolower($realm['ru']).'/'.\str_replace(' ', '+', $guild);
+        $this->battlenetUrl = 'https://'.$region.'.api.battle.net/wow/guild/'.\ucfirst($realm['ru']).'/'.\str_replace(' ', '%20', $guild).'?fields=news&locale='.$lang.'&apikey='.$apikey;
+        $this->raiderioPayload = [
+            'realmId' => $realm['id'],
+            'realm' => $realm['en'],
+            'region' => $region,
+            'guild' => $guild,
+            'numMembers' => 0, //amount of members to update. 0 = all
+        ];
     }
 
+    /**
+     * Get news.
+     *
+     * @return array
+     */
     public function get(): array
     {
         return $this->getWowprogress()
@@ -47,76 +73,88 @@ class News
                     ->data;
     }
 
-    public function update(): News
+    /**
+     * Update guild profiles.
+     *
+     * @return array
+     */
+    public function update(): self
     {
-        return $this->updateWowprogress();
+        return $this->updateWowprogress()
+                    ->updateRaiderio();
     }
 
-    protected function log(string $task = 'News', string $message): void
+    protected function log(string $task, string $message): void
     {
-        echo "[".date('Y-m-d H:i:s')."] $task - $message\n";
+        echo '['.\date('Y-m-d H:i:s')."] $task - $message\n";
     }
 
     /**
-     * Fetch data from URL
+     * Fetch data from URL.
+     *
      * @param string $url URL to GET
+     *
      * @return string
      */
     protected function fetch(string $url): string
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $data = curl_exec($ch);
-        curl_close($ch);
+        $ch = \curl_init();
+        \curl_setopt($ch, CURLOPT_URL, $url);
+        \curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = \curl_exec($ch);
+        \curl_close($ch);
 
         return $data;
     }
 
     /**
      * Send POST request to URL with DATA.
+     *
      * @param string $url
-     * @param array $data
+     * @param array  $data
+     *
      * @return string
      */
     protected function send(string $url, array $data): string
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); //workaround for redirect bug (send post - redirect - send get)
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($ch, CURLOPT_POSTREDIR, 3); //workarond for redirect bug
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $data = curl_exec($ch);
-        curl_close($ch);
+        $ch = \curl_init();
+        \curl_setopt($ch, CURLOPT_URL, $url);
+        \curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); //workaround for redirect bug (send post - redirect - send get)
+        \curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        \curl_setopt($ch, CURLOPT_POSTFIELDS, \http_build_query($data));
+        \curl_setopt($ch, CURLOPT_POSTREDIR, 3); //workarond for redirect bug
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = \curl_exec($ch);
+        \curl_close($ch);
 
         return $data;
     }
 
     /**
-     * Load news from WoWProgress
+     * Load news from WoWProgress.
+     *
      * @param int $limit Items limit
+     *
      * @return News
      */
-    protected function getWowprogress(int $limit = 5): News
+    protected function getWowprogress(int $limit = 5): self
     {
         try {
-        $raw = $this->fetch('https://wowprogress.com/rss/'.$this->wowprogressUrl);
-        $rss = simplexml_load_string($raw);
-        foreach($rss->channel->item as $item) {
-            $type = 'member-'.((strpos($item->title, 'joined') !== false) ? 'join' : 'leave');
-            $this->data[] = [
-                'timestamp' => strtotime((string)$item->pubDate),
-                'type' => $type,
-                'title' => strtr((string)$item->title, ['joined' => 'вступил(-а) в', 'left' => 'покинул(-а)']),
-                'description' => '',
-            ];
-        }
-        $this->log('FETCH WoWProgress', 'success');
-        $this->sort();
-        $this->data = array_slice($this->data, 0, $limit);
+            $raw = $this->fetch('https://wowprogress.com/rss/'.$this->wowprogressUrl);
+            $rss = \simplexml_load_string($raw);
+            foreach ($rss->channel->item as $item) {
+                $type = 'member-'.((false !== \strpos((string) $item->title, 'joined')) ? 'join' : 'leave');
+                $this->data[] = [
+                    'timestamp' => \strtotime((string) $item->pubDate),
+                    'type' => $type,
+                    'title' => \strtr((string) $item->title, ['joined' => 'вступил(-а) в', 'left' => 'покинул(-а)']),
+                    'description' => '',
+                ];
+            }
+            $this->log('FETCH WoWProgress', 'success');
+            $this->sort();
+            $this->data = \array_slice($this->data, 0, $limit);
         } catch (\Throwable $t) {
             $this->log('FETCH WoWProgress', 'fail. '.$t->getMessage());
         }
@@ -125,31 +163,32 @@ class News
     }
 
     /**
-     * Update WoWProgress results
+     * Update WoWProgress results.
+     *
      * @return News
      */
-    protected function updateWowprogress(): News
+    protected function updateWowprogress(): self
     {
-        /**
+        /*
          * Algo:
          * 1. Get char ids from wowprogress page (find by css class)
          * 2. Send POST request with those char ids
          */
         try {
-        $charIds = [];
-        $html = $this->fetch('https://wowprogress.com/update_progress/'.$this->wowprogressUrl);
-        libxml_use_internal_errors(true);
-        $dom = new \DomDocument();
-        $dom->loadHTML($html);
-        $finder = new DomXPath($dom);
-        $classname = "char_chbx";
-        $nodes = $finder->query("//*[contains(@class, '$classname')]");
-        foreach($nodes as $node) {
-            $charIds[] = substr($node->getAttribute('id'), 6); //because id is "check_1232145", so we must remove that prefix
-        }
+            $charIds = [];
+            $html = $this->fetch('https://wowprogress.com/update_progress/'.$this->wowprogressUrl);
+            \libxml_use_internal_errors(true);
+            $dom = new \DomDocument();
+            $dom->loadHTML($html);
+            $finder = new DomXPath($dom);
+            $classname = 'char_chbx';
+            $nodes = $finder->query("//*[contains(@class, '$classname')]");
+            foreach ($nodes as $node) {
+                $charIds[] = \substr($node->getAttribute('id'), 6); //because id is "check_1232145", so we must remove that prefix
+            }
 
-        $result = json_decode($this->send('https://wowprogress.com/update_progress/'.$this->wowprogressUrl, ['submit' => 1, 'char_ids' => json_encode($charIds)]), true);
-        $this->log('UPDATE WoWProgress', ($result['success'] ?? false) == true ? 'success' : 'fail');
+            $result = \json_decode($this->send('https://wowprogress.com/update_progress/'.$this->wowprogressUrl, ['submit' => 1, 'char_ids' => \json_encode($charIds)]), true);
+            $this->log('UPDATE WoWProgress', ($result['success'] ?? false) === true ? 'success' : 'fail');
         } catch (\Throwable $t) {
             $this->log('UPDATE WoWProgress', 'fail. '.$t->getMessage());
         }
@@ -158,20 +197,39 @@ class News
     }
 
     /**
-     * Load news from Battle.net API
+     * Update Raider.io guild profile.
+     *
      * @return News
      */
-    protected function getBattlenet(): News
+    protected function updateRaiderio(): self
     {
-        $news = json_decode($this->fetch($this->battlenetUrl), true);
-        if(!($news['news'] ?? false)) {
+        try {
+            $result = \json_decode($this->send('https://raider.io/api/crawler/guilds', $this->raiderioPayload), true);
+            $this->log('UPDATE Raider.io', ($result['success'] ?? false) === true ? 'success' : 'fail');
+        } catch (\Throwable $t) {
+            $this->log('UPDATE Raider.io', 'fail. '.$t->getMessage());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Load news from Battle.net API.
+     *
+     * @return News
+     */
+    protected function getBattlenet(): self
+    {
+        $news = \json_decode($this->fetch($this->battlenetUrl), true);
+        if (!($news['news'] ?? false)) {
             $this->log('FETCH Battle.net', 'fail. No news found');
+
             return $this;
         }
-        foreach($news['news'] as $item) {
-            if($item['type'] == 'guildAchievement') {
+        foreach ($news['news'] as $item) {
+            if ('guildAchievement' === $item['type']) {
                 $this->data[] = [
-                    'timestamp' => substr($item['timestamp'], 0, -3), //weird battle.net result with "000" ending in any timestamp
+                    'timestamp' => \substr((string) $item['timestamp'], 0, -3), //weird battle.net result with "000" ending in any timestamp
                     'type' => 'achievement',
                     'title' => $item['achievement']['title'],
                     'description' => $item['achievement']['description'],
@@ -179,23 +237,27 @@ class News
             }
         }
         $this->log('FETCH Battle.net', 'success');
+
         return $this;
     }
 
     /**
-     * Sort results
+     * Sort results.
+     *
      * @return News
      */
-    protected function sort(): News
+    protected function sort(): self
     {
-        uasort($this->data, function(array $a, array $b){
-            if ($a['timestamp'] == $b['timestamp']) {
+        \uasort($this->data, function (array $a, array $b) {
+            if ($a['timestamp'] === $b['timestamp']) {
                 return 0;
             }
+
             return ($a['timestamp'] < $b['timestamp']) ? 1 : -1;
         });
 
         $this->log('SORT', 'success');
+
         return $this;
     }
 }
@@ -203,14 +265,14 @@ class News
  * Run it!
  */
 // 1. Download and parse news
-$news = new News('eu', 'Галакронд', 'Ясный Лес', getenv('BATTLENET_API_KEY'), 'ru_RU');
+$news = new news('eu', ['ru' => 'Галакронд', 'en' => 'Galakrond', 'id' => 607], 'Ясный Лес', \getenv('BATTLENET_API_KEY'), 'ru_RU');
 $data = $news->update()->get();
 // 2. Create CSV file with headers
-$fp = fopen('./_data/news.csv', 'w');
-fputcsv($fp, ['timestamp', 'type', 'title', 'description']);
+$fp = \fopen('./_data/news.csv', 'w');
+\fputcsv($fp, ['timestamp', 'type', 'title', 'description']);
 // 3. And append results.
-foreach($data as $item) {
-    fputcsv($fp, $item);
+foreach ($data as $item) {
+    \fputcsv($fp, $item);
 }
-fclose($fp);
+\fclose($fp);
 // 4. Done
