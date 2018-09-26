b3813644ed6c1e14892fca8a8a52b2ce4ece7725
Nikita Chernyi <me+github@rakshazi.cf>
2018-09-26 11:36:46 +0300

    Refacoring (#3)
    * init refactoring
    
    * refactored

---
+++ updater.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * PSR-4 autoloader
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+\spl_autoload_register(function ($class): void {
+    $prefix = 'Rakshazi\\WoW\\';
+    $base_dir = __DIR__.'/updater/';
+    $len = \strlen($prefix);
+    if (0 !== \strncmp($prefix, $class, $len)) {
+        return;
+    }
+    $relative_class = \substr($class, $len);
+    $file = $base_dir.\str_replace('\\', '/', $relative_class).'.php';
+    if (\file_exists($file)) {
+        require $file;
+    }
+});
+
+$updater = new \Rakshazi\WoW\Updater(
+    'eu',
+    ['ru' => 'Галакронд', 'en' => 'Galakrond', 'id' => 607],
+    'Ясный Лес',
+    \getenv('BATTLENET_API_KEY'),
+    [
+        'zone_id' => \getenv('CF_ZONE_ID'),
+        'email' => \getenv('CF_API_EMAIL'),
+        'key' => \getenv('CF_API_KEY'),
+    ],
+    'ru_RU');
+
+if (($argv[1] ?? false) === 'news') {
+    $updater->updateProgress();
+    $updater->toCsv(\getcwd().'/_data/news.csv');
+} else {
+    $updater->purgeCache();
+}
---
+++ updater/Updater.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rakshazi\WoW;
+
+/**
+ * Guild info updater.
+ */
+class Updater extends Updater\Base
+{
+    /**
+     * Updater configuration.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Battle.net handler.
+     *
+     * @var Updater\BattleNet
+     */
+    protected $bnet;
+
+    /**
+     * WoWProgress handler.
+     *
+     * @var Updater\WowProgress
+     */
+
+    /**
+     * Raider.io handler.
+     *
+     * @var Updater\RaiderIO
+     */
+    protected $wowprogress;
+
+    /**
+     * CloudFlare handler.
+     *
+     * @var Updater\CloudFlare
+     */
+    protected $cloudflare;
+
+    /**
+     * Init.
+     *
+     * @param string $region           WoW region, default: eu
+     * @param array  $realm            WoW realm (server) with en and ru and id, example: ['ru' => 'Галакронд', 'en' => 'Galakrond', id => 607]
+     * @param string $guild            Guild name
+     * @param string $battlenet_apikey Battle.net API key
+     * @param array  $cloudflare       CloudFlare config ['zone_id' => '', 'email' => '', 'key' => '']
+     * @param string $lang             Battle.net locale, default: en_GB
+     */
+    public function __construct(string $region, array $realm, string $guild, string $battlenet_apikey = '', array $cloudflare = [], string $lang = 'en_GB')
+    {
+        $this->config = [
+            'region' => $region,
+            'realm' => $realm,
+            'guild' => $guild,
+            'api' => [
+                'battle.net' => $battlenet_apikey,
+                'cloudflare' => $cloudflare,
+            ],
+            'lang' => $lang,
+        ];
+        $this->bnet = new Updater\BattleNet($this->config);
+        $this->wowprogress = new Updater\WowProgress($this->config);
+        $this->raiderio = new Updater\RaiderIO($this->config);
+        $this->cloudflare = new Updater\CloudFlare($this->config);
+    }
+
+    /**
+     * Get latest news.
+     *
+     * @return array
+     */
+    public function getNews(): array
+    {
+        $news = \array_merge($this->bnet->getNews(), $this->wowprogress->getNews());
+
+        return $this->sort($news);
+    }
+
+    /**
+     * Update game progress.
+     */
+    public function updateProgress(): void
+    {
+        // Update guild progress
+        $this->wowprogress->updateGuild();
+        $this->raiderio->updateGuild();
+
+        // Update characters progress
+        $chars = $this->bnet->getCharacters();
+        $this->wowprogress->updateCharacters($chars);
+        $this->raiderio->updateCharacters($chars);
+    }
+
+    /**
+     * Purge website cache on cloudflare.
+     */
+    public function purgeCache(): void
+    {
+        $this->cloudflare->purgeCache();
+    }
+
+    /**
+     * Get news and save them to csv.
+     *
+     * @param string $file Path to file
+     */
+    public function toCsv(string $file): void
+    {
+        $data = $this->getNews();
+        $fp = \fopen($file, 'w');
+        \fputcsv($fp, ['timestamp', 'type', 'title', 'description']);
+        foreach ($data as $item) {
+            \fputcsv($fp, $item);
+        }
+        \fclose($fp);
+    }
+}
