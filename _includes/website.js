5648d5d29642f3d3f943fde4b67ef86f668fd9d9
Nikita Chernyi <developer.nikus@gmail.com>
2018-11-09 11:59:33 +0200

    updated ui, added streams, redesigned home page

---
+++ _includes/website.js
@@ -0,0 +1,19 @@
+//Table sort
+$(function () {
+    $('table').tablesorter({
+        theme: "blackice",
+        widthFixed: true,
+        widgets: ["filter"],
+    })
+});
+
+{% if page.layout == 'home' %}
+$(function () {
+    $('#armory-calendar').tooltip();
+    {% if site.rt.setup.modifiable %}$('#raid-setup').tooltip();{% endif %}
+    $('#raidform').on('click', function(e){
+        $('#navbarCollapse').collapse('hide')
+    });
+});
+{% endif %}
+
