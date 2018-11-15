9bc264299bcd8f4186019a3a3802d93c19ecf0d4
Nikita Chernyi <developer.nikus@gmail.com>
2018-11-15 12:20:34 +0200

    optimizations

--- _includes/website.js
+++ _includes/website.js
@@ -16,3 +16,20 @@ $(function () {
 });
 {% endif %}
 
+var whTooltips = {
+    colorLinks: true,
+    iconizeLinks: true,
+    renameLinks: true,
+    iconSize: 'small'
+};
+
+var disqus_config = function () {
+    this.page.url = "{{ site.url }}{{ site.baseurl }}{{ page.url | replace:'index.html','' | replace:'.html','' }}";
+    this.page.identifier = "{{ page.url | replace:'index.html','' | replace:'.html','' }}";
+};
+
+(function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://{{ site.disqus }}.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());(d.head || d.body).appendChild(s);
+})();
